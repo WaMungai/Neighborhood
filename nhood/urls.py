a266e6451a7d6b7a1e7491da99b50e1a9797a135
Joyce <wamungai29@gmail.com>
2020-02-21 01:57:57 +0300

    Create a URLConf that will search for business location.

--- nhood/urls.py
+++ nhood/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.home,name='home'),
     url(r'^searchbusiness/', views.search_neighborhood, name='search_neighborhood'),
+    url(r'^searchlocation',views.search_business, name="search_business"),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
