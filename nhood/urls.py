73b1379de5cad3dc742ec4ce4045627a427cc9cf
Joyce <wamungai29@gmail.com>
2020-02-20 17:17:00 +0300

    Create URLConf for searching for business in neighborhoood.

--- nhood/urls.py
+++ nhood/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home,name='home'),
+    url(r'^searchbusiness/', views.search_neighborhood, name='search_neighborhood'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
