31e3453eb91a11478f41e6b939467e08b5f0e7c4
Joyce <wamungai29@gmail.com>
2020-02-20 13:56:25 +0300

    Create test functionality for the save method.

--- nhood/models.py
+++ nhood/models.py
@@ -10,6 +10,12 @@ class Neighborhood(models.Model):
     numberofpeople=models.IntegerField(blank=True,default=0)
     hoodlocation=models.CharField(max_length=30)
     
+    def __str__(self):
+        return self.hoodname
+    
+    def savehood(self):
+        self.save()
+    
 class UserProfile(models.Model):
     profile_pic=models.ImageField(upload_to='images/',blank=True)
     editor=models.OneToOneField(User,on_delete=models.CASCADE)
@@ -19,7 +25,7 @@ class UserProfile(models.Model):
 class Business(models.Model):
     businesspic=models.ImageField(upload_to='images/',blank=True)
     businessname=models.CharField(max_length=30)
-    editor=models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
+    editor=models.ForeignKey(User,on_delete=models.CASCADE)
     area=models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
     businessemail=models.CharField(max_length=30)
     pub_date=models.DateTimeField(auto_now_add=True)
--- nhood/tests.py
+++ nhood/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Neighborhood,Business,News,UserProfile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class NeighborhoodTestClass(TestCase):
+    def setUp(self):
+        self.newhood=Neighborhood(hoodpic='pic.jpg',hoodname='SportsDrive',numberofpeople='123',hoodlocation='13thstreet')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.newhood,Neighborhood))
+        
+    def test_save_hood(self):
+        self.newhood.savehood()
+        hoods=Neighborhood.objects.all()
+        self.assertTrue(len(hoods)>0)
