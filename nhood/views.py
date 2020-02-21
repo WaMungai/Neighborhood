8b7ff8ffc9b7c2c0db1066f567f51f573406002c
Joyce <wamungai29@gmail.com>
2020-02-21 11:12:41 +0300

    Create template file for the profile.

--- nhood/views.py
+++ nhood/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import News,Neighborhood,Business,UserProfile
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return HttpResponse('News from your neighbourly neighborhood')
